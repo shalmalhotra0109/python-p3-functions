d19e2e75cd6d4394a136b2b64ae4a42b667fff8e
Shal Malhotra <smalhotra0109@gmail.com>
2023-11-13 23:37:51 -0500

    passed tests

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+    
 
-def greet(name):
-    pass
-
+def greet(name = "Naureen"):
+    print(f"Hello, {name}!")
+    
+# function greetWithDefault(name = "programmer") {
+#   console.log(`Hello, ${name}!`);
 def greet_with_default(name="programmer"):
-    pass
-
+    print(f"Hello, {name}!")
+    
+#     function add(num1, num2) {
+#   return num1 + num2;
 def add(num1, num2):
-    pass
+    return num1 + num2
+
+# function halve(number) {
+#   if (typeof number !== "number") return null;
+
+#   return number / 2;
+
+# lib_test.py
 
 def halve(number):
-    pass
+    return number / 2
+
